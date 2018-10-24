3319c050292dae9401da4adbca70ee8a9211a472
arturomf94 <arturomf94@gmail.com>
2018-10-24 11:08:42 -0500

    finish script

--- 2/clase_24_oct.pl
+++ 2/clase_24_oct.pl
@@ -1,5 +1,73 @@
 :- dynamic wm/1
 
-:- op(900, xfx, if)
-:- op(800, xfx, then)
-:- op(700, xfy, and)
+:- op(900, xfx, if).
+:- op(800, xfx, then).
+:- op(700, xfy, and).
+
+% EXAMPLE RULES
+
+% rule1
+% if a and b and c then d
+%
+% rule2
+% if a and b then e
+
+
+wm(a).
+wm(b).
+wm(c).
+wm(d).
+wm(e).
+
+init :-
+    assert(already_fired(null,false)),
+    retract(already_fired(_, _)),
+    assert(already_fired(null,false)), !.
+
+run :-
+    init,
+    exec.
+
+exec :-
+    repeat,
+    select_rule(R),
+    fire(R), !.
+
+select_rule(SelectedRule) :-
+    findall(Rule, can_fire(Rule), Candidates),
+    resolve(Candidates, SelectedRule).
+
+can_fire(Rulename if Condition then Conclusion) :-
+    Rulename if Condition then Conclusion,
+    not(already_fired(Rulename, Condition)),
+    satisfied(Condition).
+
+satisfied(A and B) :-
+    wm(A),
+    satisfied(B).
+
+satisfied(A) :- wm(A).
+
+resolve([], []).
+resolve([X|_], X).
+
+fire(Rulename if Condition then Conclusion) :-
+    !,
+    assert(already_fired(Rulename, Condition)),
+    add_town(Conclusion),
+    fail.
+
+fire(_).
+
+add_to_wm(A and B) :-
+    !,
+    assert_if_not_present(A),
+    add_to_wm(B).
+
+add_to_wm(A) :-
+    assert_if_not_present(A).
+
+assert_if_not_present(A) :- wm(A), !.
+
+assert_if_not_present(A) :-
+    assert(wm(A)).
