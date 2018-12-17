fe66794b77cf342010a68cbe41ddec55e23de63b
arturomf94 <arturomf94@gmail.com>
2018-12-17 14:46:22 -0600

    Get rid of redundant code

--- proyecto-final/sudoku_gui.py
+++ proyecto-final/sudoku_gui.py
@@ -29,33 +29,6 @@ def print_maze(arr):
             if(m<=80):
                 E = entries[m]
                 m+=1
-# 
-# def find_empty_location(arr,l):
-#     for row in range(9):
-#         for col in range(9):
-#             if(arr[row][col]==0):
-#                 l[0]=row
-#                 l[1]=col
-#                 return True
-#     return False
-# def used_in_row(arr,row,num):
-#     for i in range(9):
-#         if(arr[row][i] == num):
-#             return True
-#     return False
-# def used_in_col(arr,col,num):
-#     for i in range(9):
-#         if(arr[i][col] == num):
-#             return True
-#     return False
-# def used_in_box(arr,row,col,num):
-#     for i in range(3):
-#         for j in range(3):
-#             if(arr[i+row][j+col] == num):
-#                 return True
-#     return False
-# def check_location_is_safe(arr,row,col,num):
-#     return not used_in_row(arr,row,num) and not used_in_col(arr,col,num) and not used_in_box(arr,row - row%3,col - col%3,num)
 
 def solve_sudoku(arr):
     solver = "problem(1, " + str(arr).replace('0','_') + ")"
