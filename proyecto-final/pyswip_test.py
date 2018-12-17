7ef1b68ba4c6fb01ef51138379b556117f2e8cf9
arturomf94 <arturomf94@gmail.com>
2018-12-17 13:17:54 -0600

    Solve different sudoku from Python

--- proyecto-final/pyswip_test.py
+++ proyecto-final/pyswip_test.py
@@ -1,6 +1,7 @@
 from pyswip import Prolog
 prolog = Prolog()
 prolog.consult("sudoku.pl")
-sudoky_query = prolog.query("problem(1, Rows), sudoku(Rows), maplist(portray_clause, Rows).")
-for soln in sudoky_query:
+prolog.assertz("problem(1, [[_,_,_, _,_,_, _,_,_], [_,_,_, _,_,3, _,8,5], [_,_,1, _,2,_, _,_,_],[_,_,_, 5,_,7, _,_,_],[_,_,4, _,_,_, 1,_,_],[_,9,_, _,_,_, _,_,_],[5,_,_, _,_,_, _,7,3],[_,_,2, _,1,_, _,_,_],[_,_,_, _,_,_, _,_,1]])")
+sudoku_query = prolog.query("problem(1, Rows), sudoku(Rows), maplist(labeling([ff]), Rows).", maxresult=1)
+for soln in sudoku_query:
     print(soln)
